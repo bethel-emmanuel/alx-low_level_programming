d0fdbeef6904d49ea4942415372286f4dafa910c
Bethel Emmanuel <emmanuelbethel01@gmail.com>
2022-04-07 15:08:41 -0700

    task k 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - starting point
+  *
+  * return: 0 for success
+  */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
